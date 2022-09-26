1bb82c69bc002597e7f0fd624ef48cd7ba0c2960
kcris <kcris@KEVIN>
2022-09-25 21:38:50 -0300

    Agregar archivos de proyecto.

---
+++ EncriptacionLogin/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace EncriptacionLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.cboEncriptacion = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(223, 90);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(104, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(223, 133);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(104, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(133, 197);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(56, 19);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(267, 197);
+            this.btnEncriptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(67, 19);
+            this.btnEncriptar.TabIndex = 5;
+            this.btnEncriptar.Text = "Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // cboEncriptacion
+            // 
+            this.cboEncriptacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEncriptacion.FormattingEnabled = true;
+            this.cboEncriptacion.Items.AddRange(new object[] {
+            "MD5",
+            "SHA512"});
+            this.cboEncriptacion.Location = new System.Drawing.Point(197, 33);
+            this.cboEncriptacion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboEncriptacion.Name = "cboEncriptacion";
+            this.cboEncriptacion.Size = new System.Drawing.Size(92, 21);
+            this.cboEncriptacion.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 287);
+            this.Controls.Add(this.cboEncriptacion);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.ComboBox cboEncriptacion;
+    }
+}
+
---
+++ EncriptacionLogin/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace EncriptacionLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string EncriptarMD5(string password)
+        {
+            var msgBytes = Encoding.ASCII.GetBytes(password);
+            var md = new MD5Cng();
+            var nuevoHash = md.ComputeHash(msgBytes);
+
+            System.Text.StringBuilder resultado = new System.Text.StringBuilder();
+            foreach (byte b in nuevoHash)
+            {
+                resultado.Append(b.ToString("x2"));
+            }
+
+            return Convert.ToString(resultado); ;
+        }
+
+        public String EncriptarSHA512(string password)
+        {
+            var msgBytes = Encoding.ASCII.GetBytes(password);
+
+            var sha = new SHA512Managed();
+            var hash = sha.ComputeHash(msgBytes);
+            System.Text.StringBuilder resultado = new System.Text.StringBuilder();
+            foreach (byte b in hash)
+            {
+                resultado.Append(b.ToString("x2"));
+            }
+
+            return Convert.ToString(resultado);
+        }
+
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            
+            if (this.cboEncriptacion.SelectedItem.Equals("MD5"))
+            {
+                MessageBox.Show(EncriptarMD5(this.txtPassword.Text), "Contraseña en MD5");
+            }
+            else
+            {
+                MessageBox.Show(EncriptarSHA512(this.txtPassword.Text), "Contraseña en SHA512");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cboEncriptacion.SelectedIndex = 0;
+            this.btnEncriptar.Enabled = false;
+            this.txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (this.cboEncriptacion.SelectedItem.Equals("MD5"))
+            {
+                if (this.txtUser.Text.Equals("gil") && this.txtPassword.Text.Equals("e99a18c428cb38d5f260853678922e03"))
+                {
+                    MessageBox.Show("Usuario y contraseña correctos bienvenido");
+                }
+                else
+                {
+                    MessageBox.Show("Usuario y conraseñas incorrectos");
+                }
+            }
+            else
+            {
+                if (this.txtUser.Text.Equals("gil") && this.txtPassword.Text.Equals("c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc"))
+                {
+                    MessageBox.Show("Usuario y contraseña correctos bienvenido");
+                }
+                else
+                {
+                    MessageBox.Show("Usuario y conraseñas incorrectos");
+                }
+            }
+            //SHA512 = c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc
+            //MD5 = e99a18c428cb38d5f260853678922e03
+        }
+    }
+}
