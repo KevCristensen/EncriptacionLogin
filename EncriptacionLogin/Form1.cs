d1e27f8bafc6cdde0dbf0df89c6585234b96bf29
Kevin <ke.cristensen@duocuc.cl>
2022-09-25 21:52:52 -0300

    Mensaje de logeo satisfactorio cambiado

--- EncriptacionLogin/Form1.cs
+++ EncriptacionLogin/Form1.cs
@@ -74,22 +74,22 @@ namespace EncriptacionLogin
             {
                 if (this.txtUser.Text.Equals("gil") && this.txtPassword.Text.Equals("e99a18c428cb38d5f260853678922e03"))
                 {
-                    MessageBox.Show("Usuario y contraseña correctos bienvenido");
+                    MessageBox.Show("Bienvenido");
                 }
                 else
                 {
-                    MessageBox.Show("Usuario y conraseñas incorrectos");
+                    MessageBox.Show("Usuario y conraseñas incorrectos!");
                 }
             }
             else
             {
                 if (this.txtUser.Text.Equals("gil") && this.txtPassword.Text.Equals("c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc"))
                 {
-                    MessageBox.Show("Usuario y contraseña correctos bienvenido");
+                    MessageBox.Show("Bienvenido");
                 }
                 else
                 {
-                    MessageBox.Show("Usuario y conraseñas incorrectos");
+                    MessageBox.Show("Usuario y conraseñas incorrectos!");
                 }
             }
             //SHA512 = c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc
